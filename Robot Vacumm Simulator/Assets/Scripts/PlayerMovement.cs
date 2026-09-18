7c598dfe6725a2b35444784f12faaca5a952e4af
Erik-Thorin <erik.thorin.stockholm@gmail.com>
2026-09-18 14:21:05 +0200

    2026-09-18
    Skapde Github med unity projektet. Även startat movement systemet för spelaren.

---
+++ Robot Vacumm Simulator/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+       
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Rotate(0, -1, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Rotate(0, 1, 0);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb.linearVelocity = player.transform.forward;
+        }
+
+    }
+
+}
